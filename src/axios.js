373349a1648eb833bd0be682bc170809022d95bf
hollywoodsie <kovalioff28@gmail.com>
2022-08-09 14:51:51 +0300

    front deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://blog-app-tech.herokuapp.com/api/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
